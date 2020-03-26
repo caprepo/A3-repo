53b689b0fbb55f6b0754600ab6e9130ef641a784
vidavid <vijayakshmi.david@capgemini.com>
2020-03-26 10:19:29 +0530

    Commit All

---
+++ Day1-Demo/src/main/java/org/cap/model/SalesClass.java
@@ -0,0 +1,12 @@
+package org.cap.model;
+
+public class SalesClass {
+	
+	int[] arr= {23,45,67,-90,2,1};
+	
+	public void print() {
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+
+}
