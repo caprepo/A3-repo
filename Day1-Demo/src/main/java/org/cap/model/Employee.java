8f6a40d94d1bf43804312b6400ce2a012111bcf2
vidavid <vijayakshmi.david@capgemini.com>
2020-03-26 14:52:30 +0530

    Employee Class Added in model package.

---
+++ Day1-Demo/src/main/java/org/cap/model/Employee.java
@@ -0,0 +1,47 @@
+package org.cap.model;
+
+public class Employee {
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private double salary;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Employee(int employeeId, String firstName, String lastName, double salary) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+	}
+	
+	
+	
+
+}
