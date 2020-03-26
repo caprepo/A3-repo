cf6d09fdf7bfaafc09bb3719f8aab6924a76473c
vidavid <vijayakshmi.david@capgemini.com>
2020-03-26 12:24:37 +0530

    Show method added in MainClass.

--- Day1-Demo/src/main/java/org/cap/boot/MainClass.java
+++ Day1-Demo/src/main/java/org/cap/boot/MainClass.java
@@ -7,5 +7,11 @@ public class MainClass {
 	public static void main(String[] arg) {
 		SalesClass obj=new SalesClass();
 		obj.print();
+		show();
+	}
+	
+	public static void show() {
+		System.out.println("Hello! this is Show Method!");
+		System.out.println("greet User - Here!");
 	}
 }
