306057aaf01f2ba05ae610092741ad1f4d0c817e
vidavid <vijayakshmi.david@capgemini.com>
2020-03-27 15:30:10 +0530

    Sales Class updated with enhanced for loop!

--- Day1-Demo/src/main/java/org/cap/model/SalesClass.java
+++ Day1-Demo/src/main/java/org/cap/model/SalesClass.java
@@ -5,8 +5,9 @@ public class SalesClass {
 	int[] arr= {23,45,67,-90,2,1};
 	
 	public void print() {
-		for(int i=0;i<arr.length;i++)
-			System.out.println(arr[i]);
+		for(int num:arr)
+			System.out.println(num);
+		System.out.println("Summation yet to calculate!");
 	}
 	
 	public void display() {
