a246bc8442b5767479a3d8dbf0a94006dc18fcb4
vidavid <vijayakshmi.david@capgemini.com>
2020-03-27 15:10:26 +0530

    Sales Class Updated.

--- Day1-Demo/src/main/java/org/cap/model/SalesClass.java
+++ Day1-Demo/src/main/java/org/cap/model/SalesClass.java
@@ -5,8 +5,13 @@ public class SalesClass {
 	int[] arr= {23,45,67,-90,2,1};
 	
 	public void print() {
+		int sum=0;
 		for(int i=0;i<arr.length;i++)
+		{
+			sum+=arr[i];
 			System.out.println(arr[i]);
+		}
+		System.out.println("Summation:" + sum);
 	}
 	
 	public void display() {
